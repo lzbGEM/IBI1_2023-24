3402f16a2dbfcc518ff6a7df1ec0ee53579285f7
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-08 11:13:38 +0800

    Update culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,10 +1,11 @@
 """
+
 The initial density is 0.05
 a=1 is used to calculate the day
-while density<100:
+while density<0.9:
      density=density*2
      a=a+1
-When density exceeds 100, a is the number of days calculated
+When density exceeds 0.9, a is the number of days calculated
 
 
 
