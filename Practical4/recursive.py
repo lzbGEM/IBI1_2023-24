1b402c4f28f35f872702f71166eb1310486e3517
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-08 16:08:02 +0800

    Update recursive.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -4,7 +4,7 @@ a=a*2+3
 calculate 5 times needs to use for
 because 5 is exclusive
 so cauculate 4 times and we can get 5 num
-"""
+"""#This is a new way to mark the pseudocode
 a=4
 print(a)
 for i in range(1,5):
