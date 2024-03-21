339b8f1d8b945c1881b902049b0cef7101ecd673
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-03-21 20:43:45 +0800

    Update Average Day.py

--- practical6/Average Day.py
+++ practical6/Average Day.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-activity={'Sleep':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1}
+activity={'Sleep':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1}#store the data use dictionary
 print(activity)
 
 
@@ -8,11 +8,11 @@ other_time=24-sum(activity.values())#calculate the time of 'other'
 activity['other']=other_time#add ['other']
 
 a="Sleep"#it can be modified
-print(f'Average time of {a} is {activity[a]} hours')
+print(f'Average time of {a} is {activity[a]} hours')#format output
 
 plt.pie(activity.values(),labels=activity.keys(),startangle = 90)#set a pie chart
 
-plt.title('Pie Chart')
+plt.title('Pie Chart')#name the whole figure
 plt.show()
 plt.clf()
 
