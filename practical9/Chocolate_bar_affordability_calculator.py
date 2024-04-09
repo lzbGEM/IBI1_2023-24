df4874a97794766f753a3cbd51bbf21ec367abb7
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-04-09 19:24:31 +0800

    Update Chocolate_bar_affordability_calculator.py

--- practical9/Chocolate_bar_affordability_calculator.py
+++ practical9/Chocolate_bar_affordability_calculator.py
@@ -1,6 +1,6 @@
 def chocolate_calculate(total_money,price):
-    number=total_money//price
-    rest_money=total_money%price
+    number=total_money//price#we can get the bar number
+    rest_money=total_money%price#we can get rest money
     print(f'You can buy {number} bar. And have {rest_money} money left')
     return number,rest_money
 total_money=int(input("All money you have"))
