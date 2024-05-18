9ce5362eb652cc4b8b4f51c49b4ff47a82d331a6
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-05-18 16:37:08 +0800

    Update culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,18 +1,7 @@
-"""
 
-The initial density is 0.05
-a=1 is used to calculate the day
-while density<0.9:
-     density=density*2
-     a=a+1
-When density exceeds 0.9, a is the number of days calculated
-
-"""#This is a new way to mark the pseudocode
-
-
-density=0.05
-a=1
-while density<0.9:
+density=0.05#The initial density is 0.05
+a=1#a=1 is used to calculate the day
+while density<0.9:#if density <0.9  code into while loop
     density*=2
     a+=1
     print(f'the cell density on day {a} is {density}')#This is formatted output in python
