072ba9d1be293b150c7f239c00e47a84cf231db9
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-05-18 16:45:02 +0800

    Update Average Day.py

--- practical6/Average Day.py
+++ practical6/Average Day.py
@@ -1,18 +1,19 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # Import the 'pyplot' module from the 'matplotlib' library for plotting graphs.
+
 activity={'Sleep':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1}#store the data use dictionary
 print(activity)
 
 
 Total=24
-other_time=24-sum(activity.values())#calculate the time of 'other'
-activity['other']=other_time#add ['other']
+other_time=24-sum(activity.values())      #calculate the time of 'other'
+activity['other']=other_time      #add ['other']
 
 a="Sleep"#it can be modified
-print(f'Average time of {a} is {activity[a]} hours')#format output
+print(f'Average time of {a} is {activity[a]} hours')  #format output
 
-plt.pie(activity.values(),labels=activity.keys(),startangle = 90)#set a pie chart
+plt.pie(activity.values(),labels=activity.keys(),startangle = 90)     #set a pie chart
 
-plt.title('Pie Chart')#name the whole figure
+plt.title('Pie Chart')   # Set the title for the pie chart.
 plt.show()
 plt.clf()
 
