ad602ab6c5583cc04b3840606560557dc6e23464
lzbGEM <161556222+lzbGEM@users.noreply.github.com>
2024-05-18 18:06:44 +0800

    Update repetitive_counter.py

--- practical8/repetitive_counter.py
+++ practical8/repetitive_counter.py
@@ -1,26 +1,16 @@
-seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-def count_repeats(seq, repeat):#set a function
-    count = 0
-    start = 0
-    while True:
-        start = seq.find(repeat, start)  #find the index of the repeat sequence from the start place(initially,start=0)
-        if start == -1:#if can't find the repeat sequence,we can finish
-            break
-        else:
-          count += 1
-          start += 1  # Move to the next position to allow overlapping matches
-    return count
 
 seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-repeat1 = 'GTGTGT'
-repeat2 = 'GTCTGT'
+def repeat_counts(seq):#define a function
+   repeat1 = 'GTGTGT'
+   repeat2 = 'GTCTGT'
+   count= 0  #count is used to count the number      
+   for i in range(len(seq)):#i start from every position in seq
+       if seq[i:i+6]==repeat1 or seq[i:i+6]==repeat2:#if 6 sequence ==repeat 1,2  count will be count once
+          count+=1
+    
+   return count #return the count
 
-count_repeat1 = count_repeats(seq, repeat1)
-count_repeat2 = count_repeats(seq, repeat2)
-
-total_count = count_repeat1 + count_repeat2
-
-print(f"Total number of repeat elements in the sequence: {total_count}")
+print(f"count totall number repeat of GTGTGT and GTCTGT is {repeat_counts(seq)}") #use this function
 
 
 
